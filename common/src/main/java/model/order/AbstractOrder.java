79c403d9c2f4713662fca079fb10dbefba60fd78
zhangxufan <sunshinezxf@hotmail.com>
2016-11-22 10:25:51 +0800

    add attributes in order model

--- common/src/main/java/model/order/AbstractOrder.java
+++ common/src/main/java/model/order/AbstractOrder.java
@@ -1,6 +1,8 @@
 package model.order;
 
 import model.Entity;
+import model.customer.Customer;
+import model.goods.AbstractGoods;
 
 /**
  * Created by sunshine on 2016/11/18.
@@ -8,5 +10,61 @@ import model.Entity;
 public abstract class AbstractOrder extends Entity {
     private String orderId;
 
+    private AbstractGoods goods;
 
+    private Customer customer;
+
+    private int quantity;
+
+    private double orderPrice;
+
+    private OrderStatus orderStatus;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public AbstractGoods getGoods() {
+        return goods;
+    }
+
+    public void setGoods(AbstractGoods goods) {
+        this.goods = goods;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(double orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
 }
---
+++ common/src/main/java/model/order/OrderStatus.java
@@ -0,0 +1,8 @@
+package model.order;
+
+/**
+ * Created by sunshine on 2016/11/22.
+ */
+public enum OrderStatus {
+    CREATED, SUBMITTED, PAYED, DISPATCHED, RECEIVED, COMMENTED, CLOSED
+}
