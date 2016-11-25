b5a6dcac5a18b0609d4aa1296012723c4e34b921
zhangxufan <sunshinezxf@hotmail.com>
2016-11-25 16:10:22 +0800

    implementation of customer query

--- dist/src/main/java/dao/CustomerDao.java
+++ dist/src/main/java/dao/CustomerDao.java
@@ -3,10 +3,13 @@ package dao;
 import model.customer.Customer;
 import model.utils.ResultData;
 
+import java.util.Map;
+
 /**
  * Created by sunshine on 2016/11/22.
  */
 public interface CustomerDao {
     ResultData insert(Customer customer);
-    
+
+    ResultData query(Map<String, Object> condition);
 }
--- dist/src/main/java/dao/impl/CustomerDaoImpl.java
+++ dist/src/main/java/dao/impl/CustomerDaoImpl.java
@@ -8,6 +8,9 @@ import model.utils.ResponseCode;
 import model.utils.ResultData;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by sunshine on 2016/11/24.
  */
@@ -19,6 +22,21 @@ public class CustomerDaoImpl extends BaseDao implements CustomerDao {
         try {
             sqlSession.insert("airburg.customer.insert", customer);
             result.setData(customer);
+        } catch (Exception e) {
+            result.setResponseCode(ResponseCode.RESPONSE_ERROR);
+            result.setDescription(e.getMessage());
+        }
+        return result;
+    }
+
+    public ResultData query(Map<String, Object> condition) {
+        ResultData result = new ResultData();
+        try {
+            List<Customer> list = sqlSession.selectList("airburg.customer.query", condition);
+            if (list.isEmpty()) {
+                result.setResponseCode(ResponseCode.RESPONSE_NULL);
+            }
+            result.setData(list);
         } catch (Exception e) {
             result.setResponseCode(ResponseCode.RESPONSE_ERROR);
             result.setDescription(e.getMessage());
