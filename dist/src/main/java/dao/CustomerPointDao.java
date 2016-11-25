a563ea3e1dddad0a91b576570323d1067ef2da38
zhangxufan <sunshinezxf@hotmail.com>
2016-11-25 22:01:30 +0800

    more implementations

--- dist/src/main/java/dao/CustomerPointDao.java
+++ dist/src/main/java/dao/CustomerPointDao.java
@@ -3,9 +3,13 @@ package dao;
 import model.points.CustomerPoint;
 import model.utils.ResultData;
 
+import java.util.Map;
+
 /**
  * Created by sunshine on 2016/11/24.
  */
 public interface CustomerPointDao {
     ResultData insert(CustomerPoint point);
+
+    ResultData query(Map<String, Object> condition);
 }
--- dist/src/main/java/dao/PointRecordDao.java
+++ dist/src/main/java/dao/PointRecordDao.java
@@ -3,9 +3,13 @@ package dao;
 import model.points.PointRecord;
 import model.utils.ResultData;
 
+import java.util.Map;
+
 /**
  * Created by sunshine on 2016/11/25.
  */
 public interface PointRecordDao {
     ResultData insert(PointRecord record);
+
+    ResultData query(Map<String, Object> condition);
 }
